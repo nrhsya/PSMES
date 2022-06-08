6d5a14a395803ae401568f3c65b2ff4cfe8e3bcb
aiman434 <106478009+aiman434@users.noreply.github.com>
2022-06-09 02:33:42 +0800

    view

--- resources/views/ManageEvaluationDetailsAndMarks/EvaluationMainPage.blade.php
+++ resources/views/ManageEvaluationDetailsAndMarks/EvaluationMainPage.blade.php
@@ -16,8 +16,8 @@
         <div style="padding:10px;">
             <a id="customButton" href="/AddEvaluation"><b>Add Evaluation</b></a>
             <a id="customButton" href="/ViewEvaluation"><b>View Evaluation</b></a>
-            <a id="customButton" href="/EditEvaluation"><b>Edit Evaluation</b></a>
-            <a id="customButton" href="/DeleteEvaluation"><b>Delete Evaluation</b></a>
+            <!--<a id="customButton" href="/EditEvaluation"><b>Edit Evaluation</b></a> -->
+            <!--<a id="customButton" href="/DeleteEvaluation"><b>Delete Evaluation</b></a> -->
             <a id="customButton" href="/SubmitMarks"><b>Submit Marks</b></a>
         </div>
 </center>
